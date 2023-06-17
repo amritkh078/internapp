9e84cc327c4426b078e219758a3798dfb4de77ad
amritkh078 <amritkhatiwada078@gmail.com>
2023-06-17 19:48:41 +0545

    routes imported

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const client = require('./config/dbcon.js');
+const internRoutes = require('./routes/internRoutes.js');
+const applicationRoutes = require('./routes/applicationRoutes.js');
 
 const app = express();
+
+app.use('/internships', internRoutes);
+app.use('/applications', applicationRoutes);
 
 app.use(express.json());
 
